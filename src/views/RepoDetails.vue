4150c4208a2c27905ef15008f5bb114c7829c550
Chizycodes <okwudirejoy@gmail.com>
2023-03-14 01:08:25 +0100

    details page

--- src/views/RepoDetails.vue
+++ src/views/RepoDetails.vue
@@ -1,5 +1,56 @@
 <template>
-    <div>Details</div>
+    <div class="w-full mt-5">
+        <div class="card bg-gradient bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h1 class="card-title uppercase text-secondary font-bold text-[24px]">{{ repo?.name }}</h1>
+
+                <div class="mt-10">
+                    <div class="flex flex-col gap-8">
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] mr-20 col-span-2">Description</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.description || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Language</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.language || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Created At</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.created_at || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Updated At</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.updated_at || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Pushed At</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.pushed_at || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Homepage</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.homepage || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Size</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.size || "-" }}</p>
+                        </div>
+
+                        <div class="grid sm:grid-cols-6 grid-cols-1 gap-2 items-start">
+                            <h2 class="text-textGreen font-bold text-[18px] col-span-2">Stargazers Count</h2>
+                            <p class="text-textGreen col-span-4">{{ repo?.stargazers_count }}</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -28,6 +79,8 @@ export default {
         onMounted(() => {
             fetchRepo();
         });
+
+        return { repo };
     },
 };
 </script>
