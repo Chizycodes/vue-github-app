3614d8435d857f9dede57f2cb81e1c3e7c556008
Chizycodes <okwudirejoy@gmail.com>
2023-03-14 02:09:07 +0100

    implement pagination

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,8 @@
     <div class="border-b border-[#354651] w-full flex justify-center fixed z-50 bg-primary top-0">
         <div class="navbar bg-base-100 max-w-[1280px] px-5">
             <div class="flex-1">
-                <a class="text-white normal-case text-xl font-bold"><span class="text-secondary">GitHub </span>Repos</a>
+                <router-link to="/" class="text-white normal-case text-xl font-bold"><span class="text-secondary">GitHub
+                    </span>Repos</router-link>
             </div>
             <div class="flex-none gap-2">
                 <!-- <div class="hidden md:block">
@@ -13,9 +14,9 @@
                     <label tabIndex="{0}" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">
                             <img src="../assets/avatar.png" />
-                    </div>
-                </label>
-                <!-- <ul tabIndex={0}
+                        </div>
+                    </label>
+                    <!-- <ul tabIndex={0}
                     class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                     <li>
                         <a class="justify-between">
--- src/views/RepoList.vue
+++ src/views/RepoList.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="w-full flex justify-center">
     <div class="md:max-w-[400px] w-full">
-        <SearchBar />
+      <SearchBar />
     </div>
   </div>
   <div class="flex flex-wrap justify-between gap-5 w-full mt-10">
-    <RepoCard
-      v-for="(repo, index) in repos"
-      :key="repo.id"
-      :index="index"
-      :repo="repo"
-    />
+    <RepoCard v-for="(repo, index) in repos" :key="repo.id" :index="index" :repo="repo" />
+  </div>
+  <div>
+    <!-- Pagination -->
+    <div class="flex justify-center mt-10">
+      <div class="flex gap-10">
+        <div @click="decreament" class="btn btn-ghost btn-circle border border-secondary">&lt;&lt;</div>
+        <div @click="increament" class="btn btn-ghost btn-circle border border-secondary">&gt;&gt;</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import axiosClient from "../axiosClient";
 import RepoCard from "../components/RepoCard.vue";
 import SearchBar from "../components/SearchBar.vue";
 
 const repos = ref([]);
+const currentPage = ref(1);
 
-const fetchRepos = async() => {
+const fetchRepos = async () => {
   await axiosClient
-    .get("/users/chizycodes/repos")
+    .get(`/users/chizycodes/repos?page=${currentPage.value}&per_page=${10}`)
     .then((res) => {
       console.log(res.data);
       repos.value = res.data;
@@ -34,7 +39,24 @@ const fetchRepos = async() => {
     });
 }
 
+const increament = () => {
+  if(repos.value.length < 10) return;
+  currentPage.value += 1;
+  // console.log(currentPage.value);
+}
+
+const decreament = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+  }
+  // console.log(currentPage.value);
+}
+
 onMounted(() => {
   fetchRepos();
 });
+
+watch(currentPage, () => {
+  fetchRepos();
+})
 </script>
