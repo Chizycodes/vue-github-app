77be87d9b9b299ebe1061a763f2b2af6775fd6fe
Chizycodes <okwudirejoy@gmail.com>
2023-03-17 13:14:02 +0100

    home page ui mod

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,24 +10,13 @@
                     <SearchBar />
                 </div> -->
 
-                <div class="dropdown dropdown-end">
+                <!-- <div class="dropdown dropdown-end">
                     <label tabIndex="{0}" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">
                             <img src="../assets/avatar.png" />
                         </div>
                     </label>
-                    <!-- <ul tabIndex={0}
-                    class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                    <li>
-                        <a class="justify-between">
-                            Profile
-                            <span class="badge">New</span>
-                        </a>
-                    </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul> -->
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,19 @@
     <div class="">
         <div class="hero min-h-screen bg-base-200">
             <div class="hero-content text-center">
-                <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Hello there</h1>
-                    <p class="py-6">To fetch your repositories, simply enter your GitHub username and our app will do the
+                <div class="max-w-xl">
+                    <div class="flex justify-center mb-5"><img src="../assets/github.svg" alt="github"></div>
+                    <h1 class="text-white normal-case text-4xl md:text-7xl font-bold"><span class="text-secondary">GitHub
+                        </span>Repos</h1>
+                    <p class="pt-6 text-sm md:text-base">To fetch your repositories, simply enter your GitHub username and our app will do the
                         rest. It's that easy!</p>
-                    <div class="form-control flex flex-row">
+                        <p class="text-sm">You can try it with my username: <span class="text-secondary">Chizycodes</span> </p>
+                    <div class="form-control mt-6 flex flex-row">
                         <input v-model="username" type="text" placeholder="Github username"
                             class="input input-bordered border-r-0 rounded-r-none border-secondary w-full" />
-                        <button className="btn btn-primary rounded-l-none border-secondary bg-secondary text-primary hover:bg-secondary hover:text-primary hover:border-secondary" @click="handleClick">Get Repos</button>
+                        <button
+                            className="btn btn-primary rounded-l-none border-secondary bg-secondary text-primary hover:bg-secondary hover:text-primary hover:border-secondary"
+                            @click="handleClick">Get Repos</button>
                     </div>
                 </div>
             </div>
